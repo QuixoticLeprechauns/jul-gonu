5fe70f53adb23707e9fe00c04aa1ec390046ec20
RaphGL <raphfl.dev@gmail.com>
2023-03-23 11:56:41 +0000

    added bundler and minifier

---
+++ snowpack.config.js
@@ -0,0 +1,16 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    src: "/",
+    dist: "/",
+  },
+
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: "es2017"
+  }
+};
